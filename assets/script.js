c8f4ca6995e5dc58ae319ce244109080bcc9d420
Christina Larsen <clarsen1782@outlook.com>
2023-09-27 21:12:54 -0700

    Added pick character type

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,3 @@
-// page with generate button
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //prompt Length of password 8 to 128 characters
@@ -7,17 +5,17 @@ const user = prompt ('Pick a Password length between 8 and 128:')
 
 const number = parseInt(user);
 //prompt Character types check lower/uppercase numeric, special
-const Uppercase = confirm('Include uppercase letters?');
-const Lowercase = confirm('Include lowercase letters?');
-const Number = confirm('Include numbers?');
-const Special = confirm('Include special characters?');
-
-/*if (!Uppercase && !Lowercase && !Number && !Special) {
-  alert("You must select at least one character type.");
-  return;
-}*/
+const upperCase = confirm('Include uppercase letters?');
+const lowerCase = confirm('Include lowercase letters?');
+const numeric = confirm('Include numbers?');
+const special = confirm('Include special characters?');
 
 //check at least 1 character type is slected
+const allChars = upperCase + lowerCase + numeric + special;
+if (!upperCase, !lowerCase, !numeric, !special) {
+  alert("You must have at least one Character Type!")
+}
+
 
 // Write password to the #password input
 function writePassword() {
