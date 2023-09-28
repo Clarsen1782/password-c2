f098a174f38e861e185619da5e6701bc4baa2a28
Christina Larsen <clarsen1782@outlook.com>
2023-09-27 20:38:51 -0700

    added prompts for length and caracters

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,23 @@
+// page with generate button
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//prompt Length of password 8 to 128 characters
+const user = prompt ('Pick a Password length between 8 and 128:')
+
+const number = parseInt(user);
+//prompt Character types check lower/uppercase numeric, special
+const Uppercase = confirm('Include uppercase letters?');
+const Lowercase = confirm('Include lowercase letters?');
+const Number = confirm('Include numbers?');
+const Special = confirm('Include special characters?');
+
+/*if (!Uppercase && !Lowercase && !Number && !Special) {
+  alert("You must select at least one character type.");
+  return;
+}*/
+
+//check at least 1 character type is slected
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +27,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+//alert to display password
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
