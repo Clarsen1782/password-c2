78d0d46a80ed5c921bdccec11abd6e89fd4c5a9a
Christina Larsen <clarsen1782@outlook.com>
2023-09-28 21:43:05 -0700

    its not working

--- assets/script.js
+++ assets/script.js
@@ -3,22 +3,35 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
   //prompt length between 8 and 128
-  const user = prompt("Please pick a password length between 8 and 128?");
-  const number = parseInt(user);
-  if (isNaN(number) || number > 8 || number < 128) {
-    break;
-  } else {a
+  const user = parseTnt(prompt("Please pick a password length between 8 and 128?"));
+  
+  if (isNaN(user) || number < 8 || number > 128) {
       alert("Please choose a number between 8 and 128.");
+      return;
   }
-  return number;
-
   //prompt uppercase, lowercase, numbers, special
+  const uppercase = confirm('Include uppercase letters?');
+  const lowercase = confirm('Include lowercase letters?');
+  const numbers = confirm('Include numbers?');
+  const special = confirm('Include special characters?');
 
+  if (!uppercase || !lowercase || !number || !special) {
+    alert("Please choose at least one character typeof.");
+    return;
+  }
   //validate input
+  const charset = (
+    (uppercase ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : '') +
+    (lowercase ? 'abcdefghijklmnopqrstuvwxyz' : '') +
+    (numbers ? '0123456789' : '') +    
+    (special ? '!@#$%^&*()_-+=<>?' : '')
+  );
 
-  //generate password per criteria
-
-  //display password
+  let password = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
 }
 
  
